0930e22c4e9f429127b863acca5023b4595ca437
Nazar.M <inazar.m@gmail.com>
2020-08-18 22:49:49 +0500

    user controller added
    usertype controller added with functionality of create, edit, update and delete and show.

---
+++ OnlineLibraryMgtSystem/Controllers/UserTablesController.cs
@@ -0,0 +1,132 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace OnlineLibraryMgtSystem.Controllers
+{
+    public class UserTablesController : Controller
+    {
+        private OnlineLibraryMgtSystemDbEntities db = new OnlineLibraryMgtSystemDbEntities();
+
+        // GET: UserTables
+        public ActionResult Index()
+        {
+            var userTables = db.UserTables.Include(u => u.EmployeeTable).Include(u => u.UserTypeTable);
+            return View(userTables.ToList());
+        }
+
+        // GET: UserTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.EmployeeTables, "EmployeeID", "FullName");
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables, "UserTypeID", "UserType");
+            return View();
+        }
+
+        // POST: UserTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserTypeID,UserName,Password,EmployeeID,IsActive")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTables.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.EmployeeTables, "EmployeeID", "FullName", userTable.EmployeeID);
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables, "UserTypeID", "UserType", userTable.UserTypeID);
+            return View(userTable);
+        }
+
+        // GET: UserTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.EmployeeTables, "EmployeeID", "FullName", userTable.EmployeeID);
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables, "UserTypeID", "UserType", userTable.UserTypeID);
+            return View(userTable);
+        }
+
+        // POST: UserTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserTypeID,UserName,Password,EmployeeID,IsActive")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.EmployeeTables, "EmployeeID", "FullName", userTable.EmployeeID);
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables, "UserTypeID", "UserType", userTable.UserTypeID);
+            return View(userTable);
+        }
+
+        // GET: UserTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTable userTable = db.UserTables.Find(id);
+            db.UserTables.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnlineLibraryMgtSystem/Controllers/UserTypeTablesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace OnlineLibraryMgtSystem.Controllers
+{
+    public class UserTypeTablesController : Controller
+    {
+        private OnlineLibraryMgtSystemDbEntities db = new OnlineLibraryMgtSystemDbEntities();
+
+        // GET: UserTypeTables
+        public ActionResult Index()
+        {
+            return View(db.UserTypeTables.ToList());
+        }
+
+        // GET: UserTypeTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTypeTable userTypeTable = db.UserTypeTables.Find(id);
+            if (userTypeTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTypeTable);
+        }
+
+        // GET: UserTypeTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserTypeTable userTypeTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTypeTables.Add(userTypeTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTypeTable);
+        }
+
+        // GET: UserTypeTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTypeTable userTypeTable = db.UserTypeTables.Find(id);
+            if (userTypeTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTypeTable);
+        }
+
+        // POST: UserTypeTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserTypeID,UserType")] UserTypeTable userTypeTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTypeTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTypeTable);
+        }
+
+        // GET: UserTypeTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTypeTable userTypeTable = db.UserTypeTables.Find(id);
+            if (userTypeTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTypeTable);
+        }
+
+        // POST: UserTypeTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTypeTable userTypeTable = db.UserTypeTables.Find(id);
+            db.UserTypeTables.Remove(userTypeTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

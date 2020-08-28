44a2a0864793331c669e9513e82ca1558285c1f1
Nazar.M <inazar.m@gmail.com>
2020-08-28 15:58:06 +0500

    PurchaseTemp Model View Added
    PurchaseTemp Class added in Model folder as model view

--- DataAccessLayer/PurTemDetailsTable.cs
+++ DataAccessLayer/PurTemDetailsTable.cs
@@ -11,12 +11,19 @@ namespace DataAccessLayer
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class PurTemDetailsTable
     {
         public int PurTemID { get; set; }
+
+        [Required(ErrorMessage="Select Book")]
         public int BookID { get; set; }
+
+        [Required(ErrorMessage = "Enter Purchase Qty")]
         public int Qty { get; set; }
+
+        [Required(ErrorMessage = "Enter Purchase Unit Price")]
         public double UnitPrice { get; set; }
     
         public virtual BookTable BookTable { get; set; }
---
+++ OnlineLibraryMgtSystem/Models/PurchaseTempMV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLibraryMgtSystem.Models
+{
+    public class PurchaseTempMV
+    {
+        public int PurTemID { get; set; }
+
+        [Required(ErrorMessage = "Select Book")]
+        public int BookID { get; set; }
+
+        [Required(ErrorMessage = "Enter Purchase Qty")]
+        public int Qty { get; set; }
+
+        [Required(ErrorMessage = "Enter Purchase Unit Price")]
+        public double UnitPrice { get; set; }
+
+    }
+}
